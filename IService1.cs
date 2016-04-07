8b9a978615f01e28abfce123902eec07b5eaf858
chaotzu <darkcore.7@gmail.com>
2016-04-07 16:53:17 -0500

    Confirmación inicial para agregar los archivos .gitIgnore y .gitAttribute predeterminados.

---
+++ IService1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Wcf.App
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetData?param={value}")]
+        string Anymethod(int value);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/TestMethod", Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json
+          )]
+        string TestMethod(CompositeType value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/Empleados", ResponseFormat = WebMessageFormat.Json)]
+        Employee[] Empleados();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/Cargar", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        String Upload(Stream Uploading);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public string Usuario { get; set; }
+        [DataMember]
+        public string Correo { get; set; }
+        [DataMember]
+        public string DeptName { get; set; }
+    }
+
+    [DataContract]
+    public class CargarArchivo
+    {
+        [DataMember]
+        public string FilePath { get; set; }
+
+        [DataMember]
+        public string FileLength { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public Stream File { get; set; }
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Wcf.App
+{    
+    public class Service1 : IService1
+    {
+        public string Anymethod(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Employee[] GetEmployees()
+        {
+            return new Employee[] 
+             {
+                  new Employee() {Usuario="101",Correo="Empleado1",DeptName="iingen"},
+                  new Employee() {Usuario="101",Correo="Empleado2",DeptName="iingen"},
+                  new Employee() {Usuario="102",Correo="Empleado3",DeptName="iinngen"}
+             };
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Employee[] Empleados()
+        {
+            return GetEmployees();
+        }
+
+
+        public string TestMethod(CompositeType value)
+        {
+            return string.Format("You entered: {0}", value.StringValue);
+        }
+
+        public String Upload(Stream Uploading)
+        {
+            String nombreArchivo = "";            
+            nombreArchivo = "test.jpg";
+            String savePath = @"c:\temp\uploads\";                 
+            CargarArchivo upload = new CargarArchivo
+            {                
+                FilePath = Path.Combine(savePath, nombreArchivo)
+            };
+
+            int length = 0;
+            try
+            {
+                using (FileStream writer = new FileStream(upload.FilePath, FileMode.Create))
+                {
+                    int readCount;
+                    var buffer = new byte[4096000];
+                    while ((readCount = Uploading.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        writer.Write(buffer, 0, readCount);
+                        length += readCount;
+                    }
+                }
+
+                upload.FileLength = length.ToString();
+                String x = upload.FilePath;
+                return x;
+            }
+            catch (Exception e)
+            {
+                return (e.ToString());
+            }
+        }
+    }
+}
